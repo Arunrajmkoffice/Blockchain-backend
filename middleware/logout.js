16ac3f77f5819cae46fba4fe427cb6e194058fb8
Arunrajmkoffice <arun@medpick.in>
2024-05-21 16:31:23 +0530

    updated

--- middleware/logout.js
+++ middleware/logout.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { authenticateToken, tokenBlacklist } = require('./authenticateToken');
 const router = express.Router();
 
-// Logout route
+
 router.post('/', authenticateToken, (req, res) => {
     const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
 
-    // Add the token to the blacklist
+
     tokenBlacklist.add(token);
 
 
--- middleware/refreshToken.js
+++ middleware/refreshToken.js
@@ -8,13 +8,13 @@ const { generateAccessToken } = require('./authenticateToken');
 router.post('/', (req, res) => {
     const refreshToken = req.body.refreshToken;
 
-    // Verify the refresh token
+
     jwt.verify(refreshToken, '123', (err, user) => {
         if (err) {
             return res.status(403).json({ message: 'Invalid refresh token' });
         }
         
-        // Refresh token is valid, generate new access token
+     
         const accessToken = generateAccessToken(user);
         res.json({ accessToken });
     });
